760fe198c065ceb3b45019e52e744cb499d8f96d
roachxiao <roachxiao@tencent.com>
2021-04-05 20:01:29 +0800

    first commit.

---
+++ flink-asyncio-redis/src/main/java/org/seepure/flink/datastream/asyncio/redis/RedissonClient.java
@@ -0,0 +1,12 @@
+package org.seepure.flink.datastream.asyncio.redis;
+
+public class RedissonClient {
+
+    public static void main(String[] args) {
+        clusterMode(args);
+    }
+
+    public static void clusterMode(String[] args) {
+
+    }
+}
